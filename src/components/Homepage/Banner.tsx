3a4c61c588ec1c8246b5d4d1e40ca3fad169af23
Tony Kovatch <96496455+Akovatch@users.noreply.github.com>
2024-12-23 09:59:25 -0500

    fix: responsive logo
    Co-authored-by: Saul Thompson <118783042+saulthompson@users.noreply.github.com>
    Co-authored-by: Clarissa Roeder <123587588+clarissaroeder@users.noreply.github.com>
    Co-authored-by: Rebecca Biancofiore <58274427+fenris55@users.noreply.github.com>

--- src/components/Homepage/Banner.tsx
+++ src/components/Homepage/Banner.tsx
@@ -14,31 +14,42 @@ function Banner() {
       height="calc(100vh - 60px)"
     >
       <Box>
-        <Image src="../../img/homepage/transparent_logo.png" maxWidth={400} my={8} mx="auto"/>
+        <Image
+          src="../../img/homepage/transparent_logo.png"
+          my={8}
+          mx="auto"
+          className="img-large"
+        />
       </Box>
       <Box>
-          <Text as="h1" fontSize="3xl" fontWeight={400} maxWidth="800px" className="banner">
-            A lightweight, self-hosted error monitoring tool designed to meet the
-            needs of small to mid-sized development teams.
-          </Text>
+        <Text
+          as="h1"
+          fontSize="3xl"
+          fontWeight={400}
+          maxWidth="800px"
+          className="banner"
+        >
+          A lightweight, self-hosted error monitoring tool designed to meet the
+          needs of small to mid-sized development teams.
+        </Text>
       </Box>
       <Box>
         <Link href="/case-study" _hover={{ textDecoration: "none" }}>
-          <Button 
-          backgroundColor="transparent"
-          border="1px solid #D1E7BA"
-          color="#D1E7BA"
-          height="4rem"
-          my={8} 
-          fontSize="20px"
-          px="24px"
-          transition="background-color 0.3s, transform 0.2s"
-          _hover={{
-            backgroundColor: "#319997",
-            border: "1px solid #319997",
-            color: "white",
-            transform: "scale(1.05)",
-          }}
+          <Button
+            backgroundColor="transparent"
+            border="1px solid #D1E7BA"
+            color="#D1E7BA"
+            height="4rem"
+            my={8}
+            fontSize="20px"
+            px="24px"
+            transition="background-color 0.3s, transform 0.2s"
+            _hover={{
+              backgroundColor: "#319997",
+              border: "1px solid #319997",
+              color: "white",
+              transform: "scale(1.05)",
+            }}
           >
             Read our Case Study
           </Button>
